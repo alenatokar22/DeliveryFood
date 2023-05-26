775457dbd69cda4ea3bcd1c0f60177cfb1e389a4
Alyona <alenatokar22@gmail.com>
2023-05-26 17:07:44 +0300

    correct errors

--- src/Item/StoreItem.tsx
+++ src/Item/StoreItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../scss/storeData.scss";
+import "../scss/storeItem.scss";
 
 interface StoreItemProps {
   name: string;
